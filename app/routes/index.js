0e9b34094ca193dcad45437c703eb3b1b2334087
Prashish Rajbhandari <prashishh@gmail.com>
2015-04-02 11:32:36 -0400

    Routes: rendering index and partials.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,11 @@
 'use strict'
 
 module.exports = function(app) {
-	
+	app.get('/partials/:name', function(req, res) {
+		res.render('partials/' + req.params.name);
+	});
+
+	app.get('/', function(req, res) {
+		res.render('index');
+	});
 };

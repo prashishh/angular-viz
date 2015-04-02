487e931ee8999f01499da1bb9e31484965e1d7b6
Prashish Rajbhandari <prashishh@gmail.com>
2015-04-02 14:29:00 -0400

    Bugfix: watching server and client files fixed

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 // Main Dependencies
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+var concat_vendor = require('gulp-concat-vendor');
 var gulpLoadPlugins = require('gulp-load-plugins');
 
 var plugins = gulpLoadPlugins();
@@ -11,7 +12,7 @@ var plugins = gulpLoadPlugins();
 var clientDir = './client/';
 var publicDir = './public/';
 var clientJSFiles = clientDir + '**/*.js';
-var clientCSSFiles = clientDir + 'css/**/*.css';
+var clientCSSFiles = clientDir + 'assets/css/**/*.css';
 var serverFiles = './app/**/*.js';
 var testFiles = '';
 
@@ -40,6 +41,20 @@ gulp.task('js', function() {
   );
 });
 
+// Bundle vendor
+gulp.task('vendor', function() {
+  var start = Date.now();
+  gutil.log('Building vendor.js bundle');
+  return (
+		gulp.src('public/lib/*.js')
+				.pipe(concat_vendor('vendor.js'))
+		  	.pipe(gulp.dest(publicDir + 'js'))
+				.pipe(plugins.notify(function () {
+	      	gutil.log('vendor.js bundle built in ' + (Date.now() - start) + 'ms');
+	      }))
+  );
+});
+
 // Bundle CSS Task
 gulp.task('css', function() {
   var start = Date.now();
@@ -59,19 +74,20 @@ gulp.task('css', function() {
 
 // Watch files for changes
 gulp.task('watch', function() {
-	gulp.watch(clientJSFiles, ['lint', 'js', 'css']);
+	gulp.watch([clientJSFiles, 'app/views/*.js', 'app/views/**/*.js'], ['lint', 'js', 'css']);
 });
 
 // Nodemon Tasks
 gulp.task('nodemon', function() {
 	plugins.nodemon({
 		script: 'app.js', 
-		watch: serverFiles,
+		watch: ['gulpfile.js', 'app.js', 'app/**/*.js'],
 	})
-	.on('change', ['watch'])
+	.on('change', ['build'])
 	.on('restart', function() {
 		gutil.log('Nodemon restarted');
 	});
 });
 
-gulp.task('build', ['lint', 'css', 'js', 'nodemon']);
+gulp.task('build', ['lint', 'css', 'vendor', 'js', 'watch']);
+gulp.task('start', ['lint', 'css', 'vendor', 'js', 'watch', 'nodemon']);

a0cb46681a30de3683a6e818f4194614f5f76041
Prashish Rajbhandari <prashishh@gmail.com>
2015-04-02 14:28:28 -0400

    First proper build - angular, material design

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,44 @@
 'use strict';
 
-var logisticsApp = angular.module('dashboardApp', ['ngRoute', 'ngResource'])
-		.config(function($routeProvider, $locationProvider) {
+var dashboardApp = angular.module('dashboardApp', ['ngRoute', 'ngResource', 'ngMaterial'])
+		.config(function($routeProvider, $locationProvider, $mdThemingProvider) {
+
+			// change default color palette to deep orange
+			$mdThemingProvider.theme('default')
+				.primaryPalette('deep-orange');
 
 			$routeProvider
-				.when('/home', {
+				.when('/', {
 					templateUrl: 'partials/home',
-					controller: 'homeController'
+					controller: 'HomeController'
 				})
 				.otherwise({
-					redirectTo: '/home'
+					redirectTo: '/'
 				});
 
 			$locationProvider.html5Mode({
 				enabled: true,
 				requireBase: false
 			});
-	})
+	});
+'use strict';
+
+/* global dashboardApp, c3 */ 
+
+dashboardApp.
+  controller('homeController', function ($scope, $http, dataService, $timeout) {
+  	
+  });
+
+'use strict';
+
+/* Service */
+
+/* global dashboardApp */ 
+dashboardApp.factory('dataService', function($http) {
+	return {
+		getData : function(callback) {
+			$http.get('/api/data').success(callback);
+		}
+	};
+});

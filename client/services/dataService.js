71ae28609c28ef56164ebdf4bf48cc1592f0c046
Prashish Rajbhandari <prashishh@gmail.com>
2015-04-02 20:04:53 -0400

    Array update for c3.js in services.

--- client/services/dataService.js
+++ client/services/dataService.js
@@ -6,7 +6,15 @@
 dashboardApp.factory('dataService', function($http) {
 	return {
 		getData : function(callback) {
-			$http.get('/api/data').success(callback);
+			var new_data = [];
+			$http.get('/api/data').success(function(data, status, headers, config) {
+				new_data.push('data1');
+
+        for (var i = 0; i < data.length; i++)
+          new_data.push(parseInt(data[i]));
+				
+				callback(new_data);
+			});
 		}
 	};
 });

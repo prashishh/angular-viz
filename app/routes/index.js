fb08651f3e3c59597856a41380605c4ec87ad981
Prashish Rajbhandari <prashishh@gmail.com>
2015-04-02 20:03:19 -0400

    Added /api routes in the index.js file.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,11 +1,23 @@
 'use strict';
 
 module.exports = function(app) {
+	
+	// view rendering
 	app.get('/partials/:name', function(req, res) {
 		res.render('partials/' + req.params.name);
 	});
 
 	app.get('/', function(req, res) {
 		res.render('index');
+	});
+
+
+	// api
+	app.get('/api/data', function(req, res) {
+		var data = [];
+		for (var i = 0; i < 10; i++)
+			data.push(Math.floor(Math.random() * 10) + 1);
+		
+		res.status(200).send(data);
 	});
 };
